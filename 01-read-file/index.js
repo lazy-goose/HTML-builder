924155be705b057bf11d9a8adb362d0232e4cd9f
Lazy Goose <shamal.ma.rss@gmail.com>
2023-05-05 10:01:07 +0300

    feat: rewrite 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,11 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const process = require('process');
 
-fs.createReadStream(path.join(__dirname, 'text.txt'))
-  .on('data', (data) => {
-    process.stdout.write(data);
-  })
-  .on('end', () => {
-    // If we don’t put the break line at the end we will get a weird character after our string
-    process.stdout.write('\n');
-  });
+fs
+    .createReadStream(path.join(__dirname, 'text.txt'))
+    .on('end', () => process.stdout.write('\n'))
+    .pipe(process.stdout)
